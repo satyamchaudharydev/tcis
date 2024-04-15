c72efc65e87c07d58c6b3f8dd63a4ca92bafa136
sanatankc <sanatan.iit@gmail.com>
2024-04-15 10:11:18 +0530

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   site: "https://www.ssrbm.com/",
     integrations: [sitemap(), react(), tailwind()],
-    output: 'static',
+    output: 'hybrid',
     adapter: cloudflare({
         imageService: 'compile'
     }),
